a4c22479db6384ceb29e85570d37c1fe0b65bb82
almi1221 <name@email.comaliaksandr.miranovich@netcracker.com>
2022-12-19 14:07:58 +0300

    Hello controller created

---
+++ src/main/java/by/alex/jenkins/HelloController.java
@@ -0,0 +1,15 @@
+package by.alex.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class HelloController {
+
+    @GetMapping("hello")
+    public String sayHello() {
+        return "Hello!";
+    }
+}
